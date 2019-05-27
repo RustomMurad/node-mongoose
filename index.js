e0a54bbf35b39270211e994065fb8c044c3e1316
RustomMurad <roman.lagunov@anaplan.com>
2019-05-26 23:06:54 -0700

    Mongoose Part 3

--- index.js
+++ index.js
@@ -2,31 +2,44 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url ='mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url,{useNewUrlParser: true});
 
 connect.then((db) => {
-    console.log('Connected correctly to server');
+    console.log('Connected successfully to database.');
 
     Dishes.create({
-        name: 'Uthappizza',
-        description: 'test'
+        name: 'Uthapizza',
+        description: 'test description'
     })
     .then((dish) => {
-            console.log(dish);
+        console.log(dish);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
+        return Dishes.findOneAndUpdate(dish._id, {
+            $set: { description: 'Updated Test'}
+        }, {
+            new: true 
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
 
+        return dish.save();
+    }).then((dish) => {
+        console.log(dish);
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
